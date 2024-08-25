2cae51dcc4f3728dc7bf16acbe6f3d47c2bdd30f
tuannguyen <thaituan7820@gmail.com>
2024-08-25 17:05:37 +0700

    Next.js Animated Bar Chart Example

---
+++ src/components/AttendChart.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import Image from "next/image";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Mon",
+    present: 39,
+    absent: 0,
+  },
+  {
+    name: "Tue",
+    present: 38,
+    absent: 1,
+  },
+  {
+    name: "Wed",
+    present: 39,
+    absent: 0,
+  },
+  {
+    name: "Thu",
+    present: 39,
+    absent: 3,
+  },
+  {
+    name: "Fri",
+    present: 39,
+    absent: 0,
+  },
+];
+
+const AttendChart = () => {
+  return (
+    <div className="bg-white rounded-lg p-4 h-full">
+      {/* <div className="">
+        <h1>Attendance</h1>
+        <Image src={"/moreDark.png"} alt="" width={20} height={20} />
+      </div> */}
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Attendance</h1>
+        <Image src="/moreDark.png" alt="more" width={20} height={20} />
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart width={500} height={300} data={data} barSize={20}>
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#d1d5db" }}
+            tickLine={false}
+          />
+          <YAxis axisLine={false} tick={{ fill: "#d1d5db" }} tickLine={false} />
+          <Tooltip
+            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+          />
+          <Legend
+            align="left"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+          />
+          <Bar
+            dataKey="present"
+            fill="#FAE27C"
+            // activeBar={<Rectangle fill="pink" stroke="blue" />}
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+          <Bar
+            dataKey="absent"
+            fill="#C3EBFA"
+            // activeBar={<Rectangle fill="gold" stroke="purple" />}
+            legendType="circle"
+            radius={[10, 10, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+export default AttendChart;
