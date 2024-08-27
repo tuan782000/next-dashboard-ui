e31768a7c8672897d0cbe1356019eaa6454c8e6b
tuannguyen <thaituan7820@gmail.com>
2024-08-27 09:28:54 +0700

    Xong phần cấu hình cho từng trang

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4">
       {/* SEARCH BAR */}
-      <div className="hidden md:flex items-center gap-2 text-sx rounded-full ring-[1.5px] ring-gray-300 px-2">
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="Search" width={14} height={14} />
         <input
           type="text"
