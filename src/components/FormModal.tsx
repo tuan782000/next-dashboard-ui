4a6f1daab73071de2c284400a66f1d2c0221f762
tuannguyen <thaituan7820@gmail.com>
2024-08-27 23:49:53 +0700

    Next.js Lazy Loading Import Tutorial with dynamic()

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
-import TeacherForm from "./forms/TeacherForm";
-import StudentForm from "./forms/StudentForm";
+// import TeacherForm from "./forms/TeacherForm";
+// import StudentForm from "./forms/StudentForm";
 
-// const forms: {
-//   [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
-// } = {
-//   teacher: (type, data) => <TeacherForm type={type} data={data} />,
-//   student: (type, data) => <StudentForm type={type} data={data} />,
-// };
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
 
 const forms: {
   [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
